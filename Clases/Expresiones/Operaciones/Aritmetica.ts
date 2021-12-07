2aa664779971101b49babbc487027207ec93ac91
Byron Alvarez <3003445890101@ingenieria.usac.edu.gt>
2021-12-06 20:58:12 -0600

    expresiones aritmeticas v1

--- Clases/Expresiones/Operaciones/Aritmetica.ts
+++ Clases/Expresiones/Operaciones/Aritmetica.ts
@@ -29,9 +29,70 @@ export class Aritmetica extends Operacion implements Expresion {
       return tipo.BOOLEAN;
     }
   }
-  getValor(controlador: Controller, ts: TablaSim, ts_u: TablaSim) {}
+  getValor(controlador: Controller, ts: TablaSim, ts_u: TablaSim) {
+    let valor_expre1;
+    let valor_expre2;
+    let valor_U;
+    if (this.expreU === false) {
+      valor_expre1 = this.expre1.getValor(controlador, ts, ts_u);
+      valor_expre2 = this.expre2.getValor(controlador, ts, ts_u);
+    } else {
+      valor_U = this.expre1.getValor(controlador, ts, ts_u);
+    }
+
+    switch (this.operador) {
+      case Operador.SUMA:
+        if (typeof valor_expre1 === "number") {
+          if (typeof valor_expre2 === "number") {
+            return valor_expre1 + valor_expre2;
+          }
+        }
+        break;
+      case Operador.RESTA:
+        if (typeof valor_expre1 === "number") {
+          if (typeof valor_expre2 === "number") {
+            return valor_expre1 - valor_expre2;
+          }
+        }
+        break;
+      case Operador.MULT:
+        if (typeof valor_expre1 === "number") {
+          if (typeof valor_expre2 === "number") {
+            return valor_expre1 * valor_expre2;
+          }
+        }
+        break;
+      case Operador.DIV:
+        if (typeof valor_expre1 === "number") {
+          if (typeof valor_expre2 === "number") {
+            return valor_expre1 / valor_expre2;
+          }
+        }
+        break;
+      case Operador.MODULO:
+        if (typeof valor_expre1 === "number") {
+          if (typeof valor_expre2 === "number") {
+            return valor_expre1 % valor_expre2;
+          }
+        }
+        break;
+      default:
+        break;
+    }
+  }
 
   recorrer(): Nodo {
-    return null;
+    let padre = new Nodo("Exp", "");
+
+    if (this.expreU) {
+      padre.addHijo(new Nodo(this.op_string, ""));
+      padre.addHijo(this.expre1.recorrer());
+    } else {
+      padre.addHijo(this.expre1.recorrer());
+      padre.addHijo(new Nodo(this.op_string, ""));
+      padre.addHijo(this.expre2.recorrer());
+    }
+
+    return padre;
   }
 }
